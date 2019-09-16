9b2d1687e6a43a473af20dca76f0dba39e351ae3
surisureshhh <30382510+surisureshhh@users.noreply.github.com>
2019-09-16 12:52:18 +0530

    Add files via upload

--- challenge.ts
+++ challenge.ts
@@ -9,7 +9,7 @@ async function randomDelay() {
     return sleep(randomTime)
 }
 
-class ShipmentSearchIndex {
+export class ShipmentSearchIndex {
     async updateShipment(id: string, shipmentData: any) {
         const startTime = new Date()
         await randomDelay()
@@ -29,3 +29,27 @@ class ShipmentSearchIndex {
 interface ShipmentUpdateListenerInterface {
     receiveUpdate(id: string, shipmentData: any)
 }
+
+  export class ShipmentUpdate implements ShipmentUpdateListenerInterface {
+    private updateQueue: string[];
+  
+    constructor(private shipmentSearchIndex: ShipmentSearchIndex) {
+      this.updateQueue = [];
+    }
+  
+    async receiveUpdate(id: string, shipmentData: any) {
+      if (this.updateQueue.includes(id)) {
+        console.log('this update is already in queue');
+        return;
+      }
+      this.updateQueue.push(id);
+      try {
+        const processTime = await this.shipmentSearchIndex.updateShipment(id, shipmentData);
+        console.log('Shipment Updated \n', { id, ...shipmentData, ...processTime });
+      } catch(e) {
+        console.log('Shipment update failed', e.stack);
+      }
+      this.updateQueue = this.updateQueue.filter(idx => idx !== id);
+      console.log(this.updateQueue);
+    }
+  }
